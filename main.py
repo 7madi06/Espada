a3b726f9ad9e97e9522bdabc71aeadaaec7b459c
Youssef Saddouki <63870260+7madi06@users.noreply.github.com>
2022-11-18 23:34:08 +0100

    Revert "add ping command"
    This reverts commit b448c1cff6d8735d416a9df70c3272bab1dc2212.

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
+import random
 import discord
-from discord.ext import commands
-
-intents = discord.Intents.default()
-intents.message_content = True
-bot = commands.Bot(command_prefix='!', intents=intents)
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-bot.run('MTA0MzE1NDQ4NDA0MzQ2ODgyMA.Ggd6NX.zBa1ZxyUx3KeRHxUJsxeGuN1LgvmPo3rAsBgRs')
+import time
