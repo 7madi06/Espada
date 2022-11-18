b448c1cff6d8735d416a9df70c3272bab1dc2212
Youssef Saddouki <63870260+7madi06@users.noreply.github.com>
2022-11-18 23:32:37 +0100

    add ping command

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-import random
 import discord
-import time
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
+bot.run('MTA0MzE1NDQ4NDA0MzQ2ODgyMA.Ggd6NX.zBa1ZxyUx3KeRHxUJsxeGuN1LgvmPo3rAsBgRs')
