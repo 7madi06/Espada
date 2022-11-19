23b055604c9096569d08e810adf991fd1863097c
Youssef Saddouki <63870260+7madi06@users.noreply.github.com>
2022-11-19 12:20:02 +0100

    chanegd discord.py to hikari cause it still maintained implemented slash commands

--- main.py
+++ main.py
@@ -1,48 +1,64 @@
 import os
 from dotenv import load_dotenv
-import discord
-from discord.ext import commands
-from discord import app_commands
+import hikari
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
+bot = hikari.GatewayBot(token=TOKEN)  
 
-intents = discord.Intents.all()
-intents.message_content = True
+# Set this to the guild to create the slash commands in.
+# To create global commands, set to `hikari.UNDEFINED`.
+#
+# It is important to note that global commands can take up to an hour to show in the client
+# and should only be used when "publishing" commands after they are built. For testing
+# use guild based commands as they are instant.
+COMMAND_GUILD_ID = GUILD
 
-client = discord.Client(intents=intents)
-# -- sum configs
-client = commands.Bot(command_prefix = '.',intents = discord.Intents.all())
 
-# -- events 
-@client.event
-async def on_ready(): # when bot is online
-  print('Connected to') # it will say 'Connected to (bot name here)'
-  print(client.user)
+@bot.listen()
+async def register_commands(event: hikari.StartingEvent) -> None:
+    """Register ping and info commands."""
+    application = await bot.rest.fetch_application()
 
-# -- commands
+    commands = [
+        bot.rest.slash_command_builder("ping", "Get the bot's latency."),
+        bot.rest.slash_command_builder("info", "Learn something about the bot."),
+        bot.rest.slash_command_builder("ephemeral", "Send a very secret message."),
+    ]
 
-# - ping command example
-@client.command()
-async def ping(ctx):
-  # bot sending the message
-  await ctx.reply('Pong! {0}'.format(round(client.latency, 1)))
+    await bot.rest.set_application_commands(
+        application=application.id,
+        commands=commands,
+        guild=COMMAND_GUILD_ID,
+    )
 
-# - embed example
-@client.command()
-async def embed(ctx):
-  embed = discord.Embed(
-    color = 0x000000,
-    title = 'Embed Title!',
-    description = 'Embed Description'
-)
-  # adding fields 
-  # Field is kinda similar to title and description, for me at least.
-  embed.add_fields(name = 'Field Name', value = 'Field Value / Desc')
-  # bot sending the embed
-  await ctx.reply(embed = embed)
 
-# -- running the bot
-client.run(TOKEN)
-# replace BOT_TOKEN with your bots token
+@bot.listen()
+async def handle_interactions(event: hikari.InteractionCreateEvent) -> None:
+    """Listen for slash commands being executed."""
+    if not isinstance(event.interaction, hikari.CommandInteraction):
+        # only listen to command interactions, no others!
+        return
+
+    if event.interaction.command_name == "ping":
+        await event.interaction.create_initial_response(
+            hikari.ResponseType.MESSAGE_CREATE,
+            f"Pong! {bot.heartbeat_latency * 1_000:.0f}ms",
+        )
+
+    elif event.interaction.command_name == "info":
+        await event.interaction.create_initial_response(
+            hikari.ResponseType.MESSAGE_CREATE,
+            "Hello, this is an example bot written in hikari!",
+        )
+
+    elif event.interaction.command_name == "ephemeral":
+        await event.interaction.create_initial_response(
+            hikari.ResponseType.MESSAGE_CREATE,
+            "Only you can see this, keep it a secret :)",
+            flags=hikari.MessageFlag.EPHEMERAL,
+        )
+
+
+bot.run()
